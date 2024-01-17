786e1cfb877c9fd1e22e611c6f51ade8c997865a
Joao <joao.pparella@sp.senac.br>
2024-01-16 22:22:35 -0300

    adição swagger

--- src/filme/dto/insereFilme.dto.ts
+++ src/filme/dto/insereFilme.dto.ts
@@ -1,22 +1,44 @@
-import { IsEmail, IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsInt, IsNotEmpty, IsString } from "class-validator";
 
 
 
 export class criaFilmeDTO{
     @IsString()
     @IsNotEmpty({message: "nome Não pode ser vazio"})
+    @ApiProperty({
+        example: 'Indiana Jones',
+        description: `O nome é usado para identificar o filme e listar.`,
+    })
     nome:string;
     
     @IsInt()
-    duracao: BigInteger;
+    @ApiProperty({
+        example: '120',
+        description: `A duração é um parametro numerico que armazena quantos minutos tem o filme.`,
+    })
+    duracao: number;
 
     @IsString()
+    @ApiProperty({
+        example: 'O filme conta a história de um arqueólogo....',
+        description: `Uma breve descrição ou a sinopse completa do filme.`,
+    })
     sinopse: string;
 
-    @IsString()    
+    @IsString()  
+    @ApiProperty({
+        example: '2012',
+        description: `O ano de lançamento do filme.`,
+    })  
     ano: string;
 
     @IsString()
+    @ApiProperty({
+        example: 'Ação',
+        description: `Descrição do genero do filme`,
+    })
     genero: string;
 
 }
+
