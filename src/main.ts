17258edeb7ee26010f95e3f2c77506cc03b39521
Joao <joao.pparella@sp.senac.br>
2024-01-17 21:03:14 -0300

    removendo https

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   };
 
   
-  const app = await NestFactory.create(AppModule,{httpsOptions});
+  const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(
     new ValidationPipe({
